1e632981ca9fe4de799b0aa7984fbfc04ab351cd
jonah1234567 <jonahgarmaise@gmail.com>
2022-03-29 16:16:43 -0400

    oscillator.py mostly finished into testing phase

--- feature_matching/feature_matching_main.py
+++ feature_matching/feature_matching_main.py
@@ -4,6 +4,7 @@ import yaml
 from yaml.loader import SafeLoader
 from PIL import Image
 import numpy as np
+from feature_matching.oscillator import oscillate, m_norm_diff
 
 # Open the file and load the file
 with open('C:\\Users\\jonah\\Desktop\\Projects\\Programming\\Personal\\sliceSLAM\\config.YAML') as f:
@@ -11,17 +12,20 @@ with open('C:\\Users\\jonah\\Desktop\\Projects\\Programming\\Personal\\sliceSLAM
 
 combined_data = pd.read_csv(cfg.get("combined_data_path"))
 
-#for i in range(len(combined_data) - 1):
 
-for i in range(50,51):
+combined_data["x_offset"] = ""
+combined_data["y_offset"] = ""
+
+
+for i in range(len(combined_data) - 1):
+    print(i)
     image_A = Image.open(combined_data.loc[i, "frame_path"]).convert("L")  # L grayscale
     image_B = Image.open(combined_data.loc[i + 1, "frame_path"]).convert("L")
 
-    image_A_Data = np.asarray(image_A, dtype="float32") # maybe do ints instead?
+    image_A_Data = np.asarray(image_A, dtype="float32")  # maybe do ints instead?
     image_B_Data = np.asarray(image_B, dtype="float32")
-    print(np.shape(image_A_Data))
-    diff = image_A_Data - image_B_Data  # elementwise for np array
-    m_norm = sum(sum(abs(diff)))  # Manhattan norm
+    x_off, y_off = oscillate(image_A_Data, image_B_Data)
+    combined_data.at[i, "x_offset"] = x_off
+    combined_data.at[i, "y_offset"] = y_off
 
-
-
+combined_data.to_csv(cfg.get("combined_data_path"))
--- feature_matching/oscillator.py
+++ feature_matching/oscillator.py
@@ -1,20 +1,63 @@
 import numpy as np
+from numba import jit, cuda
 
 
 # move the second image not the first image
 
-def oscillator(im1, im2):
+@jit(forceobj=True, target_backend='CUDA')
+def oscillate(im1, im2):
     min_diff = 9999999999
     best_x, best_y = 0, 0
-    for x in range(-50, 51, 10):  # this is pure jank
+    for x in range(-50, 51, 10):  # this is pure jank, don't do grid search in the feature, or at least at first
         for y in range(-50, 51, 10):
-            im1_x, im1_y, im2_x, im2_y = 0, 0, 0, 0
-            if x > 0:
 
-            temp_diff = m_norm_diff(im1[], im2)
+            im_xi, im_xf, im_yi, im_yf = 0, 0, 0, 0  # better way to orient the different images?
+            if x > 0:
+                im_xi, im_xf = x, 0
+            elif x < 0:
+                im_xi, im_xf = 0, x
+
+            if y > 0:
+                im_yi, im_yf = y, 0
+            elif y < 0:
+                im_yi, im_yf = 0, y
+
+            im1_x, im1_y = np.shape(im1)
+            im2_x, im2_y = np.shape(im2)
+
+            temp_diff = m_norm_diff(im1[0 + im_xi:im1_x + im_xf, 0 + im_yi:im1_y + im_yf],
+                                    im2[0 - im_xf:im2_x - im_xi, 0 - im_yf:im2_y - im_yi])  # have fun
+            # understanding this later
+
             if temp_diff < min_diff:
                 min_diff = temp_diff
                 best_x, best_y = x, y
+
+    for x in range(best_x - 5, best_x + 6, 1):  # double-check the 6
+        for y in range(best_y - 5, best_y + 6, 1):  # double-check the 6
+
+            im_xi, im_xf, im_yi, im_yf = 0, 0, 0, 0  # better way to orient the different images?
+            if x > 0:
+                im_xi, im_xf = x, 0
+            elif x < 0:
+                im_xi, im_xf = 0, x
+
+            if y > 0:
+                im_yi, im_yf = y, 0
+            elif y < 0:
+                im_yi, im_yf = 0, y
+
+            im1_x, im1_y = np.shape(im1)
+            im2_x, im2_y = np.shape(im2)
+
+            temp_diff = m_norm_diff(im1[0 + im_xi:im1_x + im_xf, 0 + im_yi:im1_y + im_yf],
+                                    im2[0 - im_xf:im2_x - im_xi, 0 - im_yf:im2_y - im_yi])  # have fun
+            # understanding this later
+
+            if temp_diff < min_diff:
+                min_diff = temp_diff
+                best_x, best_y = x, y
+
     return best_x, best_y
 
 
